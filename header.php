33044c8a9ae02b0f980fda88c79b49d126458d7d
Azad <azad@azad.com>
2020-02-19 04:09:26 +0600

    Optimized

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
                                     if(has_custom_logo()){
                                         the_custom_logo();                             
                                     }else{ ?>
-                                        <h1><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+                                        <a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
                                     <?php }
                                 ?>
                             </h1>
